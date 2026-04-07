e3f0024993dca1a923ed26c6e9d48b3cbbf32418
Torsten Böhm <astroboehm@gmail.com>
2026-04-07 17:41:31 +0200

    toto

--- my_setupfiles/settings_sirius.py
+++ my_setupfiles/settings_sirius.py
@@ -1,6 +1,6 @@
 import settings_narval_vega2018
 
-class SettingsSirius(settings_narval_vega2018.SettingsReference):
+class SettingsSirius(settings_narval_vega2018.SettingsNarval2018):
     
     STARNAME_MAP_FOR_BERV = {
         'SIRIUSA': 'SIRIUS',
