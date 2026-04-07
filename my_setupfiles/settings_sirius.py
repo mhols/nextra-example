b471b43ed01de8f722511c263c2bcf7b50fe9dd1
Matthias Holschneider <matthias.holschneider@gmail.com>
2026-04-07 12:06:35 +0200

    progressing

--- my_setupfiles/settings_sirius.py
+++ my_setupfiles/settings_sirius.py
@@ -2,4 +2,8 @@ import settings_narval_vega2018
 
 class SettingsSirius(settings_narval_vega2018.SettingsReference):
     
-    STARNAME_FOR_BERV   = "SIRIUS"
+    STARNAME_MAP_FOR_BERV = {
+        'SIRIUSA': 'SIRIUS',
+        'ALDEBARAN_QUATSCH': 'ALDEBARAN'   
+    }
+     
