11da424904176657551323e3f06079c1e293314d
Torsten Böhm <astroboehm@gmail.com>
2026-08-17 17:48:43 +0200

    toto

---
+++ my_setupfiles/settings_sirius_espadons.py
@@ -0,0 +1,31 @@
+import os
+import numpy as np
+from dotenv import load_dotenv
+
+import nextra.units as nu
+import nextra.continuum as continuum
+
+
+
+comment = {}  ## this comments will be included into the .fits document
+
+
+class SettingsSiriusEspadons(nx.settings_espadons.SettingsReferenceEspadons):
+    IS_REFERENCE = False
+    VOIE_METHOD = "OPTIMAL_EXTRACT"
+
+
+
+
+def get_kwargs():
+    from nextra import settings_espadons
+    tmp = settings_espadons.get_kwargs()
+    tmp.update({
+        k: v for k, v in SettingsSiriusEspadons.__dict__.items() if not k.startswith("_")
+    })
+    return tmp
+
+
+## the following parameters are included into the fits files header
+PREFIX = "NEXTRA_"
+HEADER_ITEMS = [k for k in comment.keys() if k[:2] != "__"]
